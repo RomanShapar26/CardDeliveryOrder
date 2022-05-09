36504d89a5bc6f19e8344d25e0bef18640b8879a
Роман Шапарь <romanshaparution@gmail.com>
2022-05-09 20:04:43 +0300

    added

--- src/test/java/ru.netology/CardDeliveryOrderTest.java
+++ src/test/java/ru.netology/CardDeliveryOrderTest.java
@@ -8,13 +8,26 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
     @BeforeEach
     public void openPage() {
         open("http://localhost:9999/");
+    }
+
+    @Test
+    void schouldRegistredAccount() {
+        open("http://localhost:9999/");
+        $("placeholder=\"Город\"").setValue("Краснодар");
+        $("name=\"phone\"").setValue("+79912345678")
+        $("name=\"name\"").setValue("Иван Иванов");
+        $("data-test-id=\"agreement\"").click();
+        $("//*[text()=\"Забронировать\"]").click();
+        $(withText("Встреча успешно забронирована на ")).should(visible);
+
     }
 
 }
