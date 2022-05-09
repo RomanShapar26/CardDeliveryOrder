463352fc1a698eb08aa908d868db245de4b57f74
Роман Шапарь <romanshaparution@gmail.com>
2022-05-09 19:26:11 +0300

    added

---
+++ src/test/java/ru.netology/CardDeliveryOrderTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryOrderTest {
+    @BeforeEach
+    public void openPage() {
+        open("http://localhost:9999/");
+    }
+
+}
