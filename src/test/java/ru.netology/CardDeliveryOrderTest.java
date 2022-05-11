0af949ad0d7ace35fbca49cbcb7b3b177e88b0b6
Роман Шапарь <romanshaparution@gmail.com>
2022-05-11 15:23:56 +0300

    modif

--- src/test/java/ru.netology/CardDeliveryOrderTest.java
+++ src/test/java/ru.netology/CardDeliveryOrderTest.java
@@ -1,14 +1,15 @@
 package ru.netology;
+
 import com.codeborne.selenide.Condition;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,14 +21,100 @@ public class CardDeliveryOrderTest {
 
     @Test
     void schouldRegistredAccount() {
-        open("http://localhost:9999/");
-        $("placeholder=\"Город\"").setValue("Краснодар");
-        $("name=\"phone\"").setValue("+79912345678")
-        $("name=\"name\"").setValue("Иван Иванов");
-        $("data-test-id=\"agreement\"").click();
-        $("//*[text()=\"Забронировать\"]").click();
-        $(withText("Встреча успешно забронирована на ")).should(visible);
-
+        Configuration.timeout = 15000;
+        $("[placeholder='Город']").setValue("Краснодар");
+        $(".menu-item__control").click();
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+77412369852");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible);
     }
+
+
+    @Test
+    public void shouldSuccessfullyWithManualFilling() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String meetingDate = LocalDate.now().plusDays(6).format(formatter);
+        $("[placeholder='Город']").setValue("Санкт-Петербург");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иван Иванов");
+        $("[name='phone']").setValue("+77412369852");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+    }
+
+    @Test
+    public void shouldSuccessfullyWithManualFillingThreeDays() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String meetingDate = LocalDate.now().plusDays(3).format(formatter);
+        $("[placeholder='Город']").setValue("Санкт-Петербург");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(meetingDate);
+        $("[name='name']").setValue("Иван Иванов");
+        $("[name='phone']").setValue("+77412369852");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+    }
+
+    @Test
+    public void shouldNoNumberTell(){
+        Configuration.timeout = 15000;
+        $("[placeholder='Город']").setValue("Краснодар");
+        $(".menu-item__control").click();
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldNoName(){
+        Configuration.timeout = 15000;
+        $("[placeholder='Город']").setValue("Краснодар");
+        $(".menu-item__control").click();
+        $("[name='name']").setValue("");
+        $("[name='phone']").setValue("+77412369852");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+//    @Test
+//    public void shouldNoCity(){
+//        Configuration.timeout = 15000;
+//        $("[placeholder='Город']").setValue("");
+//        $(".menu-item__control").click();
+//        $("[name='name']").setValue("Иванов Иван");
+//        $("[name='phone']").setValue("+77412369852");
+//        $(".checkbox__box").click();
+//        $("[placeholder='Город']").setValue("");
+//        $(withText("Забронировать")).click();
+//        $("[data-test-id='Город'].input_invalid .input__sub").shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
+//    }
+//    @Test
+//    public void shouldShowErrorNoSymbol() {
+//        open("http://localhost:9999/");
+//        Configuration.timeout = 15000;
+//        $("[placeholder='Город']").setValue("Krasnodar");
+//        $(".menu-item__control").click();
+//        $("[data-test-id=name] input").setValue("");
+//        $("[data-test-id=phone] input").setValue("");
+//        $(".checkbox__box").click();
+//        $(withText("Забронировать")).click();
+//        $("[data-test-id=''].input_invalid .input__sub").shouldHave(Condition.exactText("Поля обязательные для заполнения"));
+//    }
+
+
+
+
 
 }
